3ebbc27b8409f3bb060cb611ed80301ff3855ef9
justanooooooooooo <justanchen2010@gmail.com>
2026-04-07 14:00:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,32 +18,52 @@ const products = [
     name: "RED BULL",
     price: 70,
     image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }
+  },
   {
     name: "特上紅茶",
     price: 35,
     image: "https://img.yec.tw/zp/MerchandiseImages/de9855922d-Gd-5169846.jpg"
-  }{
-    name: "RED BULL",
-    price: 70,
-    image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }{
-    name: "RED BULL",
-    price: 70,
-    image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }{
-    name: "RED BULL",
-    price: 70,
-    image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }{
-    name: "RED BULL",
-    price: 70,
-    image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }{
-    name: "RED BULL",
-    price: 70,
-    image: "https://city9.tw/cdn/shop/products/red-bull-940214.png?v=1701171430"
-  }
+  },
+  {
+    name: "滿漢大餐蔥燒牛肉麵",
+    price: 40,
+    image: "https://cs-a.ecimg.tw/items/DBAY10A900HSAQB/001001_1725516647.jpg"
+  },
+  {
+    name: "來一客鮮蝦魚板",
+    price: 40,
+    image: "https://shoptaiwan.us/cdn/shop/products/image_8af03874-7bfc-4751-9a3a-236e35615d3b_512x512.jpg?v=1661241883"
+  },
+  {
+    name: "來一客牛肉蔬菜",
+    price: 40,
+    image: "https://mall.iopenmall.tw/website/uploads_product/website_9920/P0992005735027_3_48745670.jpeg?hash=50348"
+  },
+  {
+    name: "來一客韓式泡菜",
+    price: 40,
+    image: "https://shoptaiwan.us/cdn/shop/products/image_b5427701-c7b8-42bd-b4f1-95468500f188_600x600.jpg?v=1661241898"
+  },
+  {
+    name: "來一客川辣牛肉",
+    price: 40,
+    image: "https://online.carrefour.com.tw/on/demandware.static/-/Sites-carrefour-tw-m-inner/default/dw73f69a96/images/large/1450104500124_NR_00.png"
+  },
+  {
+    name: "大乾麵蔥燒牛肉",
+    price: 40,
+    image: "https://eatfoodgod.com/wp-content/uploads/2024/05/c-22.webp"
+  },
+  {
+    name: "樂事原味",
+    price: 40,
+    image: "https://online.carrefour.com.tw/on/demandware.static/-/Sites-carrefour-tw-m-inner/default/dw66ab0e68/images/large/1402006800101.png"
+  },
+  {
+    name: "卡拉姆久",
+    price: 40,
+    image: "https://b2eimg.pxec.com.tw/00154559/827eedb36bbe48b2aa15fe38f4c01194.jpg"
+  },
 ];
 
 let cart = [];
